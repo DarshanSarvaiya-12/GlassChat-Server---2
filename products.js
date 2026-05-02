198961b13f903206e7b80389baf089c715311925
Darshan Sarvaiya. <darshansarvaiya818@gmail.com>
2026-05-02 08:40:19 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,36 @@
+const products = {
+  "TS01": {
+    name: "Classic T-Shirt",
+    color: "Blue",
+    price: 299,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    image_url: "https://res.cloudinary.com/do0h2fawi/image/upload/v1776542927/vuoui8gjkiydylil0ijc.png",
+    caption: "Code: TS01 | Blue Classic T-Shirt | ₹299"
+  },
+  "TS02": {
+    name: "Classic T-Shirt",
+    color: "Red",
+    price: 299,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    image_url: "https://res.cloudinary.com/do0h2fawi/image/upload/v1776542866/i18wa1vkxqep8vijfia7.png",
+    caption: "Code: TS02 | Red Classic T-Shirt | ₹299"
+  },
+  "TS03": {
+    name: "Premium T-Shirt",
+    color: "Black",
+    price: 349,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    image_url: "https://res.cloudinary.com/do0h2fawi/image/upload/v1776542821/htuepyclen5esj1fbyte.png",
+    caption: "Code: TS03 | Black Premium T-Shirt | ₹349"
+  },
+  "TS04": {
+    name: "Premium T-Shirt",
+    color: "White",
+    price: 349,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    image_url: "https://res.cloudinary.com/do0h2fawi/image/upload/v1776542793/u3xfctrj0vcmm52jcfzu.png",
+    caption: "Code: TS04 | White Premium T-Shirt | ₹349"
+  }
+};
+
+module.exports = products;
